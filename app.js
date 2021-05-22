53336fb2878f171d4aab4be3d38671f9ca2fa1c2
ヨシダタイキ <abc521fjr7@gmail.com>
2021-05-22 23:14:29 +0900

    [update]

--- app.js
+++ app.js
@@ -1,58 +1,75 @@
-const question = 'ゲーム史上、最も売れたゲーム機は次のうちどれ？';
-const answers = [
-  'スーパーファミコン', 
-  'プレステ2', 
-  'ニンテンドースイッチ', 
-  'ニンテンドーDS'
+const quiz = [
+  {
+    question: 'ゲーム史上、最も売れたゲーム機はどれ？',
+    answers: [ 'スーパーファミコン', 'PlayStation 2', 'ニンテンドーDS', 'Xbox 360'],
+    correct: 'ニンテンドーDS'
+  }, {
+    question: '糸井重里が企画に関わった、任天堂の看板ゲームといえば？',
+    answers: [ 'MOTHER2', 'スーパーマリオブラザーズ3', 'スーパードンキーコング', '星のカービィ'],
+    correct: 'MOTHER2'
+  }, {
+    question: 'ファイナルファンタジーⅣの主人公の名前は？',
+    answers: [ 'フリオニール', 'クラウド', 'ティーダ', 'セシル'],
+    correct: 'セシル'
+  }
 ];
 
-const correct = 'ニンテンドーDS';
+const $window = window;
+const $doc = document;
+const $question = $doc.getElementById('js-question');
+const $buttons = $doc.querySelectorAll('.btn');
 
-const $button = document.getElementsByTagName('button');
-const $buttonLength = $button.length;
+const quizLen = quiz.length;
+let quizCount = 0;
+let score = 0;
 
-// クイズの問題、選択肢を定義
-const setupQuiz = () => {
-  document.getElementById('js-question').textContent = question;
-  let buttonIndex = 0;
-  let buttonLength = $button.length;
-  while(buttonIndex < buttonLength){
-    $button[buttonIndex].textContent= answers[buttonIndex];
-    buttonIndex++;
-  }
-}
-setupQuiz();
-
-const clickHandler = (e) => {
-  if(correct === e.target.textContent){
-    window.alert('正解！');
-  } else {
-    window.alert('不正解！');
+const init = () => {
+  $question.textContent = quiz[quizCount].question;
+  
+  const buttonLen = $buttons.length;
+  let btnIndex = 0;
+  
+  while(btnIndex < buttonLen){
+    $buttons[btnIndex].textContent = quiz[quizCount].answers[btnIndex];
+    btnIndex++;
   }
 };
 
-// ボタンを押したら正誤判定
-let handlerIndex = 0;
-while (handlerIndex < buttonLength) {
-  $button[handlerIndex].addEventListener('click', (e) => {
-    clickHandler(e);
+const goToNext = () => {
+  quizCount++;
+  if(quizCount < quizLen){
+    init(quizCount);
+  } else {
+    // $window.alert('クイズ終了！');
+    showEnd();
+  }
+};
+
+const judge = (elm) => {
+  if(elm.textContent === quiz[quizCount].correct){
+    $window.alert('正解!');
+    score++;
+  } else {
+    $window.alert('不正解!');
+  }
+  goToNext();
+};
+
+const showEnd = () => {
+  $question.textContent = '終了！あなたのスコアは' + score + '/' + quizLen + 'です';
+  
+  const $items = $doc.getElementById('js-items');
+  $items.style.visibility = 'hidden';
+};
+
+init();
+
+let answersIndex = 0;
+let answersLen = quiz[quizCount].answers.length;
+
+while(answersIndex < answersLen){
+  $buttons[answersIndex].addEventListener('click', (e) => {
+    judge(e.target);
   });
-  handlerIndex++;
+  answersIndex++;
 }
-
-// $button[0].addEventListener('click', (e) => {
-//   clickHandler(e);
-// });
-
-// $button[1].addEventListener('click', (e) => {
-//   clickHandler(e);
-// });
-
-// $button[2].addEventListener('click', (e) => {
-//   clickHandler(e);
-// });
-
-// $button[3].addEventListener('click', (e) => {
-//   clickHandler(e);
-// });
-
